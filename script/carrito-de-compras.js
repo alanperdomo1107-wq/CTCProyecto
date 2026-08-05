1a36dd1050abe26aa91f2c6c4dad0de87a6b3a8b
Alan Perdomo <alanperdomo1107@gmail.com>
2026-08-05 13:20:21 -0300

    Reapply "Corrigiendo errores en otra rama"
    This reverts commit 5621e976e8fb727978a255280e946fa96bc4ed12.

--- script/carrito-de-compras.js
+++ script/carrito-de-compras.js
@@ -223,7 +223,8 @@ function botonComprar(){
 		}
     
     compras.push(compra);
-    
+    localStorage.setItem('productosRegistrados',JSON.stringify(productosRegistrados));
+
     localStorage.setItem('compras',JSON.stringify(compras));
 
     mostrarCarrito()
